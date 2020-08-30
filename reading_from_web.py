34791df726bde2f83bf5b8d6a81743e87d6b097b
Umair Alam <umairalam@outlook.com>
2020-08-30 23:24:10 +0500

    Create reading_from_web.py
    Reading website data, Web scraping and usage of BeautifulSoup

---
+++ reading_from_web.py
@@ -0,0 +1,34 @@
+'''
+Reading website data, Web scraping
+usage of BeautifulSoup
+'''
+
+
+import re
+import urllib.request
+import urllib.parse
+import urllib.error
+from bs4 import BeautifulSoup as bs
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+if len(url) < 1 : url = 'http://py4e-data.dr-chuck.net/comments_362967.html'
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = bs(html, 'html.parser')
+num_list = list()
+count = 0
+#separate all span tags from the data
+span_tag = soup('span')
+for spans in span_tag:
+    x = re.findall('[0-9]+', str(spans)) #to mitigate the error "TypeError: expected string or bytes-like object" while..
+    #searching <span as it was starting with "<" tag
+    count +=1
+    for num in x:
+        num_list.append(int(num))
+print("Count", count)
+print("Sum", sum(num_list))
